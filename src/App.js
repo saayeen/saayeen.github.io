a34487bbdc8560267e99a1368d4684dfdc0527db
saaieen <sayenbarra@gmail.com>
2026-03-30 09:25:29 -0300

    kjasbndgf

---
+++ src/App.js
@@ -0,0 +1,31 @@
+function App() {
+  return (
+    <div style={{
+      fontFamily: "Arial",
+      textAlign: "center",
+      background: "#0f172a",
+      color: "white",
+      minHeight: "100vh",
+      paddingTop: "50px"
+    }}>
+      
+      <h1>Hola, soy Sayen 👋</h1>
+      <p>Esta es mi página hecha con React</p>
+
+      <button style={{
+        marginTop: "20px",
+        padding: "10px 20px",
+        background: "#3b82f6",
+        border: "none",
+        borderRadius: "8px",
+        color: "white",
+        cursor: "pointer"
+      }}>
+        Ver más
+      </button>
+
+    </div>
+  );
+}
+
+export default App;
